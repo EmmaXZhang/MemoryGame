e6667561a5ca4807e3958579536829d3bf899133
EmmaZhang <emmazxd@gmail.com>
2024-02-14 13:00:34 +1100

    remove countdown height,width

--- menu.js
+++ menu.js
@@ -1,13 +1,13 @@
 document.getElementById("EazyMode").addEventListener("click", function () {
   btnClickSound.play();
   document.querySelector(".gameDifficulty").classList.add("hide");
-  document.getElementsByClassName("container")[0].style.display = "grid";
+
   init(DIFFICULTY.EASY);
 });
 
 document.getElementById("HardMode").addEventListener("click", function () {
   btnClickSound.play();
   document.querySelector(".gameDifficulty").classList.add("hide");
-  document.getElementsByClassName("container")[0].style.display = "grid";
+
   init(DIFFICULTY.HARD);
 });
